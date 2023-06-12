5ddd67fc208365ee55948886e9a45e880ca26976
Anurag Chaturvedi <anuragChaturvedi978@gmail.com>
2023-06-12 14:46:27 +0530

    feat: implement check on exchange owner

--- contracts/exchange-token/src/multittest.rs
+++ contracts/exchange-token/src/multittest.rs
@@ -1,15 +1,17 @@
 
 #[cfg(test)]
 mod multest {
-    use cw_multi_test::{App, ContractWrapper, Executor};
+    use cw_multi_test::{App, ContractWrapper, Executor, AppResponse};
     use crate::contract::{execute, instantiate, query};
-    use cosmwasm_std::{Addr, Empty, Uint128};
+    use cosmwasm_std::{Addr, Empty, Uint128, Response};
     use crate::msg::{ExecuteMsg::*, OrderListForERC20, OrderListForERC721};
-    use cw20_base::contract::{execute as cw20execute , query as cw20query, instantiate as cw20instantiate};
-    use cw721_base::entry::{execute as cw721execute, query as cw721query, instantiate as cw721instantiate };
-    use cw20_base::msg::{ExecuteMsg as cw20_executeMsg, InstantiateMsg as cw20_instantiateMsg};
-    use cw721_base::msg::{ExecuteMsg as cw721_executeMsg, InstantiateMsg as cw721_instantiateMsg};
-    use cw20::{Cw20Coin};
+
+    use cw20_base::{ msg::{ExecuteMsg as cw20_executeMsg, InstantiateMsg as cw20_instantiateMsg, QueryMsg as cw20_queryMsg} ,contract::{execute as cw20execute , query as cw20query, instantiate as cw20instantiate}};
+    use cw20::{Cw20Coin, BalanceResponse};
+
+    use cw721_base::{Extension , msg::{InstantiateMsg as cw721_instantiateMsg}, entry::{execute as cw721execute, query as cw721query, instantiate as cw721instantiate }};
+    use cw721::{OwnerOfResponse, Cw721QueryMsg};
+    pub type Cw721ExecuteMsg = cw721_base::ExecuteMsg<Extension, String>;
 
      #[test]
     fn multitest() {
@@ -24,66 +26,74 @@ mod multest {
         let code_cw721 = ContractWrapper::new(cw721execute, cw721instantiate, cw721query);
         let code_id_cw721 = app.store_code(Box::new(code_cw721));
 
+        let exchange_owner = Addr::unchecked("owner");
+        let seller = Addr::unchecked("seller");
+        let buyer = Addr::unchecked("buyer");
+
         let addr_cw20 = app.instantiate_contract(
             code_id_cw20,
-            Addr::unchecked("buyer"),
+            buyer.clone(),
             &cw20_instantiateMsg{
-                name: "my".to_string(),
-                symbol: "m".to_string(),
+                name: "mytoken".to_string(),
+                symbol: "myt".to_string(),
                 decimals: 3,
                 initial_balances: vec![Cw20Coin{
-                    address: "buyer".to_string(),
+                    address: buyer.to_string().clone(),
                     amount : Uint128::new(1000)
                 }],
                 mint:None, 
                 marketing: None
             },
             &[],
-            "buyer",
+            buyer.clone(),
             None
-        );
+        ).unwrap();
 
         let addr = app
         .instantiate_contract(
             code_id,
-            Addr::unchecked("owner"),
+            exchange_owner.clone(),
             &Empty {},
             &[],
-            "owner",
+            exchange_owner.clone(),
             None,
         )
         .unwrap();
 
-        let res_approve_cw20 = app.execute_contract(Addr::unchecked("buyer"), addr_cw20.unwrap(), &cw20_executeMsg::IncreaseAllowance { spender: addr.to_string(), amount: Uint128::new(200), expires: None }, &[]);
+        let res_approve_cw20 = app.execute_contract(buyer.clone(), addr_cw20.clone(), &cw20_executeMsg::IncreaseAllowance { spender: addr.to_string(), amount: Uint128::new(200), expires: None }, &[]);
 
         let addr_cw721 = app.instantiate_contract(
             code_id_cw721,
-            Addr::unchecked("seller"),
+            seller.clone(),
             &cw721_instantiateMsg{
-                name:"nft".to_string(),
-                symbol:"n".to_string(),
-                minter:"seller".to_string()
+                name:"nftToken".to_string(),
+                symbol:"nyt".to_string(),
+                minter:seller.to_string().clone()
             },
             &[],
-            "seller",
+            seller.clone(),
             None
-        );
+        ).unwrap();
 
-        let re_mint_cw721 = app.execute_contract(Addr::unchecked("seller"), 
-        addr_cw721.unwrap().clone(),
-        &cw721_executeMsg::Mint { token_id: 2.to_string(), owner: "seller".to_string(), token_uri: None, extension: None },
+        let re_mint_cw721 = app.execute_contract(seller.clone(), 
+        addr_cw721.clone(),
+        &Cw721ExecuteMsg::Mint { 
+            token_id: 2.to_string(),
+            owner: seller.clone().to_string(), 
+            token_uri: None, 
+            extension: None },
         &[]
         );
 
-        let re_approve_cw721 = app.execute_contract(Addr::unchecked("seller"), 
-        addr_cw721.unwrap().clone(),
-         &cw721_executeMsg::Approve { spender: addr.to_string(), token_id: 2.to_string(), expires: None },
+        let re_approve_cw721 = app.execute_contract(seller.clone(), 
+        addr_cw721.clone(),
+         &Cw721ExecuteMsg::Approve { spender: addr.to_string(), token_id: 2.to_string(), expires: None },
         &[],
         );
 
         let list = OrderListForERC721 {
-            owner: Addr::unchecked("seller"),
-            contract_address:addr_cw721.unwrap(),
+            owner: seller.clone(),
+            contract_address:addr_cw721.clone(),
             erc721_token_id: 2,
             amount_of_erc20_want: 200,
         };
@@ -91,44 +101,33 @@ mod multest {
             list_for_seller: list,
         };
 
-        let resp = app.execute_contract(Addr::unchecked("owner"), addr.clone(), &msg, &[]);
+        let resp = app.execute_contract(Addr::unchecked(exchange_owner.clone()), addr.clone(), &msg, &[]).unwrap();
 
         let list = OrderListForERC20 {
-            owner: Addr::unchecked("buyer"),
-            contract_address: addr_cw20.unwrap(),
+            owner: buyer.clone(),
+            contract_address: addr_cw20.clone(),
             amount_of_erc20: 200,
             erc721_token_id_want: 2,
-            erc721_contract_address: addr_cw721.unwrap()
+            erc721_contract_address: addr_cw721.clone()
         };
         let msg = Exchange {
             list_for_buyer: list,
         };
+        let resp = app.execute_contract(Addr::unchecked(exchange_owner), addr.clone(), &msg, &[]).unwrap();
 
-        let resp_s = app.execute_contract(Addr::unchecked("owner"), addr.clone(), &msg, &[]);
+        let resp:BalanceResponse = app
+        .wrap()
+        .query_wasm_smart(addr_cw20, &cw20_queryMsg::Balance { address: seller.to_string() })
+        .unwrap();
 
-        println!("{:?}", resp_s);
+        let res: OwnerOfResponse= app
+        .wrap()
+        .query_wasm_smart(
+            addr_cw721, 
+            &Cw721QueryMsg::OwnerOf { token_id: 2.to_string(), include_expired: None }
+        )
+        .unwrap();
+        assert_eq!(resp, BalanceResponse{balance: Uint128::new(200)});
+        assert_eq!(res, OwnerOfResponse { owner: buyer.to_string(), approvals: vec![]})
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // use cw20_base::contract::{execute as cw20execute, instantiate as cw20instantiate, query as cw20query};
-    // use cw721_metadata_uri::entry::{execute as cw721execute, instantiate as cw721instantiate, query as Cw721query}
-    // use cw721_base::{InstantiateMsg as cw721_instantiateMsg, ExecuteMsg as cw721_executeMsg};
-    // use cw20_base::msg::{ExecuteMsg as cw20_executeMsg, InstantiateMsg as cw20_instantiateMsg};
-    // use cw20::{Cw20Coin, MarketingInfoResponse};
-    // use cw721_base::MintMsg; 
-    // use cosmwasm_std::Coin;
